885dcf6fc2c1fe10c2dcd76f18665d90e6f53658
nicolasesanchez <nicolase.sanchez@mercadolibre.com>
2019-09-17 20:34:37 -0300

    add gitignore

---
+++ src/main/java/com/pos/web/controller/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.pos.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msg", getMessage());
+        model.addAttribute("today", new Date());
+        return "index";
+    }
+
+    private String getMessage() {
+        return "Hello World";
+    }
+}
